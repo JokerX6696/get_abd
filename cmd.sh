9e580458bf548b8b753ee5c1c4eec0ddd341703d
JokerX6696 <313445042@qq.com>
2023-10-20 14:55:55 +0800

    删除冗余数据

--- cmd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-
-python3 scripts/MAP2B.py -i {input.datalist} -o MAP2B -e {params.enzyme_site} -s {params.database} -d database/micro -p 6 -g 5 {params.host} >{log.o} 2>{log.e}
-python3 scripts/update_mapping_file_after_MAP2B.py -i MAP2B/none_micro_smp.txt -d ./ >{log.o} 2>{log.e}
