8e9ebd428c9cecb11a9077cc6db4ab9c8b49cbd9
JokerX6696 <313445042@qq.com>
2023-11-10 14:35:03 +0800

    添加 常用处理 cleandata_list 命令

---
+++ cmd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 常用处理 cleandata_list 命令
+set -e
+cleandata_dir='/storge1/automation/projects/storge1/2bRAD-M/DZOE20230778972-b1/Suppl_2/cleandata'
+paste -d '\t' \
+<(ls ${cleandata_dir}/*|sed 's|\S*/||g;s/.BcgI.fq.gz//g') \
+<(ls ${cleandata_dir}/*)  \
+> cleandata_list.txt
