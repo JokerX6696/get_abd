3f712b9df036a8d9f7b14a7dc74383cdcb3cdaa0
JokerX6696 <313445042@qq.com>
2023-12-07 15:07:13 +0800

    修复 后缀保留问题

--- cmd.sh
+++ cmd.sh
@@ -3,6 +3,6 @@
 set -e
 cleandata_dir='/storge1/automation/projects/storge1/2bRAD-M/DZOE20230778972-b1/Suppl_2/cleandata'
 paste -d '\t' \
-<(ls ${cleandata_dir}/*|sed 's|\S*/||g;s/.BcgI.fq.gz//g') \
+<(ls ${cleandata_dir}/*|sed 's|\S*/||g;s/.BcgI.fq.gz//g;s/.fq.gz//g') \
 <(ls ${cleandata_dir}/*)  \
 > cleandata_list.txt
