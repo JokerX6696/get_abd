fa44d799137617b2dad6a31ee1cf82671264e46e
JokerX6696 <313445042@qq.com>
2023-12-20 13:41:20 +0800

    目录层级修改

--- cmd.sh
+++ cmd.sh
@@ -2,7 +2,13 @@
 # 常用处理 cleandata_list 命令
 set -e
 cleandata_dir='/storge1/automation/projects/storge1/2bRAD-M/DZOE20230778972-b1/Suppl_2/cleandata'
+
 paste -d '\t' \
-<(ls ${cleandata_dir}/*|sed 's|\S*/||g;s/.BcgI.fq.gz//g;s/.fq.gz//g') \
-<(ls ${cleandata_dir}/*)  \
+<(ls ${cleandata_dir}/*/*gz|sed 's|\S*/||g;s/.BcgI.fq.gz//g;s/.fq.gz//g') \
+<(ls ${cleandata_dir}/*/*gz)  \
 > cleandata_list.txt
+
+# paste -d '\t' \
+# <(ls ${cleandata_dir}/*|sed 's|\S*/||g;s/.BcgI.fq.gz//g;s/.fq.gz//g') \
+# <(ls ${cleandata_dir}/*)  \
+# > cleandata_list.txt
